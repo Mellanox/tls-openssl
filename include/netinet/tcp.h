c363be46c0143007622e3ec35af4439a9eeb7b20
Aviad Yehezkel <aviadye@mellanox.com>
2017-05-04 13:03:49 +0300

    New uapi
    Issue: None
    Change-Id: If8f845ffa7f30c3276e09a52a1eb981e3563305c
    Signed-off-by: Aviad Yehezkel <aviadye@mellanox.com>

--- include/netinet/tcp.h
+++ include/netinet/tcp.h
@@ -258,43 +258,52 @@ struct tcp_md5sig
   u_int8_t	tcpm_key[TCP_MD5SIG_MAXKEYLEN];	/* Key (binary).  */
 };
 
+
+/****************************************************/
+/*                  TLS NEW STUFF                   */
+/****************************************************/
+#define SOL_TLS		282
+
+/* Supported versions */
+#define TLS_VERSION_MINOR(ver)	((ver) & 0xFF)
+#define TLS_VERSION_MAJOR(ver)	(((ver) >> 8) & 0xFF)
+
+#define TLS_VERSION_NUMBER(id)	((((id##_VERSION_MAJOR) & 0xFF) << 8) |	\
+				 ((id##_VERSION_MINOR) & 0xFF))
+
+#define TLS_1_2_VERSION_MAJOR	0x3
+#define TLS_1_2_VERSION_MINOR	0x3
+#define TLS_1_2_VERSION		TLS_VERSION_NUMBER(TLS_1_2)
+
 /* Supported ciphers */
-#define TLS_CIPHER_AES_GCM_128                  51
-#define TLS_CIPHER_AES_GCM_128_IV_SIZE          ((size_t)8)
-#define TLS_CIPHER_AES_GCM_128_KEY_SIZE         ((size_t)16)
-#define TLS_CIPHER_AES_GCM_128_SALT_SIZE        ((size_t)4)
-#define TLS_CIPHER_AES_GCM_128_TAG_SIZE         ((size_t)16)
+#define TLS_CIPHER_AES_GCM_128			51
+#define TLS_CIPHER_AES_GCM_128_IV_SIZE		((size_t)8)
+#define TLS_CIPHER_AES_GCM_128_KEY_SIZE		((size_t)16)
+#define TLS_CIPHER_AES_GCM_128_SALT_SIZE	((size_t)4)
+#define TLS_CIPHER_AES_GCM_128_TAG_SIZE		((size_t)16)
+#define TLS_CIPHER_AES_GCM_128_REC_SEQ_SIZE	((size_t)8)
+
+#define TLS_SET_RECORD_TYPE	1
 
 enum tls_state {
-        TLS_STATE_SW = 0x0,
-        TLS_STATE_HW = 0x1,
+	TLS_STATE_SW = 0x0,
+	TLS_STATE_HW = 0x1,
 };
 
 struct tls_crypto_info {
-        unsigned short version;
-        unsigned short cipher_type;
-        unsigned int state;
+	unsigned short version;
+	unsigned short cipher_type;
+	unsigned int state;
 };
 
-struct tls_crypto_info_aes_gcm_128 {
-        struct tls_crypto_info info;
-        unsigned char iv[TLS_CIPHER_AES_GCM_128_IV_SIZE];
-        unsigned char key[TLS_CIPHER_AES_GCM_128_KEY_SIZE];
-        unsigned char salt[TLS_CIPHER_AES_GCM_128_SALT_SIZE];
+struct tls12_crypto_info_aes_gcm_128 {
+	struct tls_crypto_info info;
+	unsigned char iv[TLS_CIPHER_AES_GCM_128_IV_SIZE];
+	unsigned char key[TLS_CIPHER_AES_GCM_128_KEY_SIZE];
+	unsigned char salt[TLS_CIPHER_AES_GCM_128_SALT_SIZE];
+	unsigned char rec_seq[TLS_CIPHER_AES_GCM_128_REC_SEQ_SIZE];
 };
-
-#define TLS_VERSION_MINOR(ver)  ((ver) & 0xFF)
-#define TLS_VERSION_MAJOR(ver)  (((ver) >> 8) & 0xFF)
-
-#define TLS_VERSION_NUMBER(id)  ((((id##_VERSION_MAJOR) & 0xFF) << 8) | \
-                                 ((id##_VERSION_MINOR) & 0xFF))
-
-#define TLS_1_2_VERSION_MAJOR   0x3
-#define TLS_1_2_VERSION_MINOR   0x3
-#define TLS_1_2_VERSION         TLS_VERSION_NUMBER(TLS_1_2)
-
-#define SOL_TLS                 282
-#define TLS_SET_RECORD_TYPE     1
+/****************************************************/
 
 #endif /* Misc.  */
 
